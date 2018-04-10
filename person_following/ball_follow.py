ccc400499b4195f53022719220d6259f6cbb1f21
Andrew Gu <andrew@mingbogu.com>
2018-04-10 15:23:53 -0400

    tracking portion done - outputs the direction needed to go

--- person_following/ball_follow.py
+++ person_following/ball_follow.py
@@ -2,10 +2,10 @@ import cv2
 import sys
 import numpy as np
 from skimage.morphology import dilation
-# import serial
-# from Adafruit_IO import Client
+import serial
+from Adafruit_IO import Client
 # ser = serial.Serial('COM4', 9600, timeout=0)
-# aio = Client('285ffbe7f2f14f9ab047842600bd13c8')
+aio = Client('37dae49e09e64368b034e523c2bce404')
 
 # Set up capture device
 cap = cv2.VideoCapture(1)
@@ -46,6 +46,8 @@ while True:
         largestContourIdx = np.argmax([len(c) for c in contours])
         #cv2.drawContours(img, contours, largestContourIdx, (0,255,0), 3)
         (x,y),radius = cv2.minEnclosingCircle(contours[largestContourIdx])
+        # print('X', x)
+        # print('Y', y)
         center = (int(x),int(y))
         #print(center)
         radius = int(radius)
@@ -64,7 +66,7 @@ while True:
 
     # Exit if q is pressed
     if cv2.waitKey(1) == ord('q'):
-        serial.write('H'.encode('ascii'))
+#        serial.write('H'.encode('ascii'))
         sys.exit(0)
         break
     
@@ -119,74 +121,76 @@ while True:
     #         if help == '0':
     #             aio.send('408i-robot-control.help', '1')
     #             help = '1'
-    
-    # # Send Directions to Arduino
-    # if command == '0':
-    #     #print('HOLD')
-    #     ser.write('H'.encode('ascii'))
-    # elif command == '1':
-    #     commandCount = 0;
-    #     #print('FOLLOW')
-    #     if(found):
-    #         xloc = int(x)
-    #         if xloc < 200: #Left
-    #             ser.write('L'.encode('ascii'))
-    #             recent = 'L'
-    #             #print('L')
-    #         elif xloc > 400: #Right
-    #             ser.write('R'.encode('ascii'))
-    #             recent = 'R'
-    #             #print('R')
-    #         else: #Forward
-    #             ser.write('F'.encode('ascii'))
-    #             #print('F')
-    #     else:
-    #         if recent == 'L': #Search Left
-    #             ser.write('Y'.encode('ascii'))
-    #             #print('Y')
-    #         elif recent == 'R': #Search Right
-    #             ser.write('X'.encode('ascii'))
-    #             #print('X')
-    # elif command == '2':
-    #     turncount = turncount + 1
-    #     if turncount < 100:
-    #         ser.write('X'.encode('ascii'))
-    #     else:
-    #         ser.write('H'.encode('ascii'))
-    # elif command == '3':
-    #     commandCount = 20
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('E'.encode('ascii'))
-    #     commandCount = 10
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('N'.encode('ascii'))
-    #     commandCount = 20
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('E'.encode('ascii'))
-    #     commandCount = 10
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('N'.encode('ascii'))
-    #     commandCount = 40
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('E'.encode('ascii'))
-    #     ser.write('N'.encode('ascii'))
-    #     commandCount = 40
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('R'.encode('ascii'))
-    #     commandCount = 80
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('L'.encode('ascii'))
-    #     commandCount = 400
-    #     while commandCount > 0:
-    #         commandCount = commandCount - 1
-    #         ser.write('R'.encode('ascii'))
-    #     ser.write('H'.encode('ascii'))
-    #     command == '0'
-    #     sys.exit(0)
+
+    # Send Directions to Arduino
+    command = '1' # fake it for now
+    if command == '0':
+        #print('HOLD')
+        ser.write('H'.encode('ascii'))
+    elif command == '1':
+        commandCount = 0;
+        #print('FOLLOW')
+        if(found):
+            xloc = int(x)
+            if xloc < 250: #Left
+#                ser.write('L'.encode('ascii'))
+                recent = 'L'
+                print('L')
+            elif xloc > 450: #Right
+#                ser.write('R'.encode('ascii'))
+                recent = 'R'
+                print('R')
+            else: #Forward
+#                ser.write('F'.encode('ascii'))
+                print('F')
+        else:
+            if recent == 'L': #Search Left
+ #               ser.write('Y'.encode('ascii'))
+                print('Y')
+            elif recent == 'R': #Search Right
+  #              ser.write('X'.encode('ascii'))
+                print('X')
+    elif command == '2':
+        turncount = turncount + 1
+
+        if turncount < 100:
+            ser.write('X'.encode('ascii'))
+        else:
+            ser.write('H'.encode('ascii'))
+    elif command == '3':
+        commandCount = 20
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('E'.encode('ascii'))
+        commandCount = 10
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('N'.encode('ascii'))
+        commandCount = 20
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('E'.encode('ascii'))
+        commandCount = 10
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('N'.encode('ascii'))
+        commandCount = 40
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('E'.encode('ascii'))
+        ser.write('N'.encode('ascii'))
+        commandCount = 40
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('R'.encode('ascii'))
+        commandCount = 80
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('L'.encode('ascii'))
+        commandCount = 400
+        while commandCount > 0:
+            commandCount = commandCount - 1
+            ser.write('R'.encode('ascii'))
+        ser.write('H'.encode('ascii'))
+        command == '0'
+        sys.exit(0)
